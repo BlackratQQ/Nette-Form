8d5210ffdfc1fcc2e205d1f2d5599a23e0336473
Vojtěch <vojtech.kochta@gmail.com>
2022-03-27 23:53:23 +0200

    Nette-form

---
+++ app/Presenters/HomepagePresenter.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+class HomepagePresenter extends Nette\Application\UI\Presenter
+{
+  protected function createComponentRegistrationForm(): Form
+  {
+    $form = new Form;
+    $form->addText('Username', 'Jméno:')
+      ->setRequired();
+
+    $form->addPassword('Password', 'Heslo:')
+      ->setRequired();
+
+    $form->addSubmit('send', 'Registrovat');
+    $form->onSuccess[] = [$this, 'commentFormSucceeded'];
+    return $form;
+  }
+  public function commentFormSucceeded(\stdClass $data): void
+  {
+       foreach ($data as $key => $value) {
+         echo $key . " je: " . $value . "<br>";
+       }
+  }
+}
+
